fd2d858dd786ea42b47ca05c79bc7048386baefd
maksim-korzhov <korzhov.m.s@yandex.ru>
2021-02-15 22:24:02 +0300

    Added Loop class

--- index.js
+++ index.js
@@ -3,8 +3,45 @@ class Loop {
     // update - to calculate new position of the rect
     // display - to draw it on canvas
     constructor(update, display) {
+        this.update = update;
+        this.display = display;
 
+        // Difference between the last update and the current time.
+        this.deltaTime = 0;
+        this.lastUpdate = 0;
+
+        // If we go to the other tab - stop animation after 40ms
+        this.maxInterval = 40;
+
+        this.animate = this.animate.bind(this);
+        this.animate();
     }
+
+    // currentTime - we get it from the requestAnimationFrame
+    animate(currentTime = 0) {
+        // Put the animate functiion in the infinite loop.
+        // This function is called on each frame if we have enough
+        // resources to process its callback.
+        // Usually number of calls depecnds on a monitor refresh rate(often 60Hz).
+        // So, when different monitors have different refresh rate or there
+        // some heavy operations in the background then it can be calles less times.
+        // It is puts "currentTime" parameter to the callback.
+        // currentTime - number of time in ms passed since we load the page.
+        requestAnimationFrame(this.animate);
+
+        // Set delta
+        this.delta = currentTime - this.lastUpdate;
+
+        // Prevent redrawing when tab is incative(if it is invisible longer than 40ms)
+        if(this.delta < this.maxInterval) {
+            // Put time in seconds to the update function.
+            this.update(this.deltaTime / 1000);
+
+            this.display(1000 / this.deltaTime | 0)
+        }
+
+        this.lastUpdate = currentTime;
+    } 
 }
 
 class Layer {
